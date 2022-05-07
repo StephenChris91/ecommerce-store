e408ee7cc8b215d9152e55648c06f112121b4aed
Stephen O. Ogbonna <stephenchris91@gmail.com>
2022-05-07 23:05:20 +0100

    finished the backend authentication and password hashing with bcrypt

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,15 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userControllers.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userControllers.js'
+import { protect } from '../data/middleware/authMiddleware.js';
 
 
 
 
-//@desc     Fetch all products
-//@route    GET /api/v1/products
-//@access   Public
-
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router;
